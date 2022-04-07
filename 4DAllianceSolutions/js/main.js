0f80ece5a200cc1edf068e6e538ffdd9ccca1b3a
iandominic <javalavama@gmail.com>
2022-04-08 03:20:17 +0800

    change

--- 4DAllianceSolutions/js/main.js
+++ 4DAllianceSolutions/js/main.js
@@ -13,16 +13,44 @@ navClose.click(function() {
 
 function scrollHeader(){
     if(this.scrollY >= 50) {
-        header.css("background-color" , "#1e5d81"); 
+        header.css("background-color" , "#1e5d81d7"); 
         header.css("box-shadow" , "0px 5px 10px rgba(0, 0, 0, 0.329)"); 
-        $('.nav__logo img').css("width", "90px");
+        header.css("backdrop-filter", "blur(4px)")
         $('.nav').css("height", "4rem");
     }
     else {
         header.css("background-color" , "transparent");
         header.css("box-shadow" , "0px 5px 20px rgba(0, 0, 0, 0)"); 
-        $('.nav__logo img').css("width", "120px");
+        header.css("backdrop-filter", "blur(0px)")
         $('.nav').css("height", "5rem");
     }
 }
 window.addEventListener('scroll', scrollHeader)
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{f
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.side__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.side__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
